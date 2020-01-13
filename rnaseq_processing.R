360a1b794fa6b80d2fd07e2886361447e46959ab
pxtm <mcg.marc@gmail.com>
2020-01-13 11:32:34 +0100

    Add files via upload
    update script

--- rnaseq_processing.R
+++ rnaseq_processing.R
@@ -1,7 +1,7 @@
-library(QuasR); library(Rsubread)
+library(QuasR); library(Rsubread); library(DESeq2)
 
-setwd('C:/Users/mcgma/Desktop/felix_seqs')
-
+## upload data
+adapter <- scan('adapter.txt', character())
 ## adapter removal and trimming
 # obtain a list of fastq file paths
 fastqFiles <- list.files(path = ".", pattern = 'fastq.gz')
@@ -15,16 +15,48 @@ outfiles <- paste("processed_", fastqFiles,".fastq",sep="")
 # remove reads shorter than 23 base-pairs (minLength)
 preprocessReads(fastqFiles, outfiles, 
                 truncateEndBases=4,
-                Lpattern="TGGAATTCTCGGGTGCCAAGG",
+                Lpattern=adapter,
                 minLength=23)
 
+## align against reference HG38 canonical
 processed <- list.files(path = ".", pattern = '.fastq$')
 
-ref <- system.file("extdata","reference.fa",package="Rsubread")
-buildindex(basename="reference_index",reference=ref)
-
-align.stat <- align(index = "./reference_index", annot.inbuilt = 'hg38', readfile1 = processed, 
-                    output_file =  paste(processed,"subread",output_format='BAM',sep="."), phredOffset = 64)
-
+# buildindex(basename="reference_index",reference='GCA_000001405.15_GRCh38_genomic.fna.gz')
+# buildindex(basename="hg38_ref",reference='references/GRCh38.primary_assembly.genome.fa.gz')
+gc()
+# align.stat <- align(index = "./reference_index", annot.inbuilt = 'GCA_000001405.15_GRCh38_genomic.fna.gz', readfile1 = processed, 
+#                     output_file =  paste(processed,"subread",output_format='BAM',sep="."), phredOffset = 33)
+align.stat <- align(index = "./hg38_ref", annot.inbuilt = 'hg38', readfile1 = processed,
+                    output_file =  paste(processed,"subread",output_format='BAM',sep="."), phredOffset = 33, nthreads = -1)
+gc()
+## extract counts and prepare count matrix
 files <- list.files(path = ".", pattern = '.BAM$')
-fc <- featureCounts(files, annot.inbuilt="hg38")
+props <- propmapped(files=files) # proportion mapped
+sink('mapped_proportions.txt')
+props
+sink()
+
+fc <- featureCounts(files, annot.inbuilt="hg38", nthreads = -1)
+# fc <- featureCounts(files, annot.ext="annotation_files/hg38_gencode.GTF", isGTFAnnotationFile=T, 
+#               nthreads=16, allowMultiOverlap=TRUE, fraction=TRUE, isPairedEnd=FALSE)
+## DESeq2 Comparison conditions
+counts <- as.data.frame(fc$counts)
+counts <- counts[]
+write.table(counts, 'rawcounts.txt', sep = '\t')
+
+mtdt <- read.table('mtdt.txt', header = T, row.names = 1)
+## clean-up samples
+dim(counts)
+counts <- counts[rowSums(counts[,-1])>0,] ## quitamos todos los reads que sean 0 para todas las muestras
+dim(counts)
+write.table(counts, 'cleanedcounts.txt', sep = '\t')
+
+dds <- DESeqDataSetFromMatrix(countData = counts, colData = mtdt, design = ~ condition) ## deseq2 normalización y pipeline
+dds <- DESeq(dds)
+results <- results(dds) ## resultados (log2FC, pvalue y adjusted pvalue)
+write.table(results, 'fullresults_deseq2.txt', sep = '\t')
+
+results.significativos <- results[which(results$padj<.05),]
+write.table(results.significativos, 'resultados_significativos.txt', sep = '\t')
+norm.counts <- counts(dds, normalized = T) ## dataframe con los counts normalizados (ILR)
+write.table(norm.counts, 'normalized_counts.txt', sep = '\t')
